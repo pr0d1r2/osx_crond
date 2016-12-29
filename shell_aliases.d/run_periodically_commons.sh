ec7b91351c71a3c069c0c1c061bf071753af73c3
Marcin Nowicki <pr0d1r2@gmail.com>
2016-12-29 11:46:57 +0100

    Use rbenv if available

--- shell_aliases.d/run_periodically_commons.sh
+++ shell_aliases.d/run_periodically_commons.sh
@@ -1,4 +1,8 @@
 function run_periodically_commons() {
   echo "#!`which zsh`"
+  if [ -d $HOME/.rbenv/bin ]; then
+    echo 'export PATH="$HOME/.rbenv/bin:$PATH"'
+    echo 'if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi'
+  fi
   echo "source $HOME/.compiled_shell_aliases.sh"
 }
