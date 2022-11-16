e0f01366b1c7641e05b725707f3085885807b1fd
Marcin Nowicki <pr0d1r2@gmail.com>
2022-11-16 17:32:27 +0100

    Fix the hour below 10

--- shell_aliases.d/run_at.sh
+++ shell_aliases.d/run_at.sh
@@ -1,13 +1,26 @@
 function run_at() {
+  local run_at_HOUR
+  case $1 in
+    [0-9]:[0-9][0-9])
+      run_at_HOUR="0$1"
+      ;;
+    [0-9][0-9]:[0-9][0-9])
+      run_at_HOUR="$1"
+      ;;
+    *)
+      return 101
+      ;;
+  esac
+
   if [ ! -d $HOME/.cron.d/at ]; then
     echo "No $HOME/.cron.d/at you need to setup osx_crond first ..."
     return 1001
   fi
-  test -d "$HOME/.cron.d/at/$1" || mkdir "$HOME/.cron.d/at/$1"
+  test -d "$HOME/.cron.d/at/$run_at_HOUR" || mkdir "$HOME/.cron.d/at/$run_at_HOUR"
 
   local run_at_FILENAME
   run_at_FILENAME="$(echo "${@:2}" | shasum | cut -f 1 -d " ")"
 
-  run_periodically_commons > "$HOME/.cron.d/at/$1/$run_at_FILENAME.sh"
-  echo "${@:2}" >> "$HOME/.cron.d/at/$1/$run_at_FILENAME.sh"
+  run_periodically_commons > "$HOME/.cron.d/at/$run_at_HOUR/$run_at_FILENAME.sh"
+  echo "${@:2}" >> "$HOME/.cron.d/at/$run_at_HOUR/$run_at_FILENAME.sh"
 }
--- shell_aliases.d/run_at_weekdays.sh
+++ shell_aliases.d/run_at_weekdays.sh
@@ -1,13 +1,26 @@
 function run_at_weekdays() {
+  local run_at_weekdays_HOUR
+  case $1 in
+    [0-9]:[0-9][0-9])
+      run_at_weekdays_HOUR="0$1"
+      ;;
+    [0-9][0-9]:[0-9][0-9])
+      run_at_weekdays_HOUR="$1"
+      ;;
+    *)
+      return 101
+      ;;
+  esac
+
   if [ ! -d $HOME/.cron.d/at ]; then
     echo "No $HOME/.cron.d/at you need to setup osx_crond first ..."
     return 1001
   fi
-  test -d "$HOME/.cron.d/at_weekdays/$1" || mkdir "$HOME/.cron.d/at_weekdays/$1"
+  test -d "$HOME/.cron.d/at_weekdays/$run_at_weekdays_HOUR" || mkdir "$HOME/.cron.d/at_weekdays/$run_at_weekdays_HOUR"
 
-  local run_at_weekdays_FILENAME
-  run_at_weekdays_FILENAME="$(echo "${@:2}" | shasum | cut -f 1 -d " ")"
+  local run_at_weekdays_weekdays_FILENAME
+  run_at_weekdays_weekdays_FILENAME="$(echo "${@:2}" | shasum | cut -f 1 -d " ")"
 
-  run_periodically_commons > "$HOME/.cron.d/at_weekdays/$1/$run_at_weekdays_FILENAME.sh"
-  echo "${@:2}" >> "$HOME/.cron.d/at_weekdays/$1/$run_at_weekdays_FILENAME.sh"
+  run_periodically_commons > "$HOME/.cron.d/at_weekdays/$run_at_weekdays_HOUR/$run_at_weekdays_weekdays_FILENAME.sh"
+  echo "${@:2}" >> "$HOME/.cron.d/at_weekdays/$run_at_weekdays_HOUR/$run_at_weekdays_weekdays_FILENAME.sh"
 }
